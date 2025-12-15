531645ac0435be84ff83d04b4d63af1d1c1128c7
Lucas-wu60ss <lucas_wu@imagtw.com>
2025-12-15 16:14:23 +0800

    測試用

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,17 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-export default defineConfig(({ mode }) => {
-	// const isProd = mode === 'production';
-
-	return {
-		base: '/n8n-test-front-end-page/',
-		plugins: [tailwindcss(), react()],
-		resolve: {
-			alias: {
-				'@': path.resolve(__dirname, './src'),
-			},
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [tailwindcss(), react()],
+	base: '/n8n-test-front-end-page/',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
 		},
-		server: {
-			host: true,
-			allowedHosts: ['localhost', '.trycloudflare.com'],
-			proxy: {
-				'/n8n': {
-					target: 'http://localhost:5678',
-					changeOrigin: true,
-					rewrite: (p) => p.replace(/^\/n8n/, ''),
-				},
-			},
-		},
-	};
+	},
+	server: {
+		host: true,
+		allowedHosts: ['localhost', '.trycloudflare.com'],
+	},
 });
